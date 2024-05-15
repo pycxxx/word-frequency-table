92f5928d79541dbc10ed9124522692aa7e19a3da
pycxxx <pyc@mykitties.life>
2024-05-15 20:42:47 +0800

    init

---
+++ transform.mjs
@@ -0,0 +1,23 @@
+import { promisify } from 'util'
+import * as path from 'path'
+import * as fs from 'fs'
+
+
+(async function main() {
+  const readFile = promisify(fs.readFile)
+  const data = await readFile(path.join(import.meta.dirname, 'BIAU1.TXT'))
+  const decoder = new TextDecoder('big5')
+  const content = decoder.decode(data)
+  const stream = fs.createWriteStream(path.join(import.meta.dirname, 'result.csv'))
+  const re = /^.*?║\s*?(\d+)\s*?│\s*?(.?)\s*?│\s*?(.+?)\s*?│\s*?(\d+)\s*?║\s*?(\d+)\s*?│\s*?(\d+)\s*?│\s*?(\d+(?:\.\d+))\s*?║/
+  content.split('\n').forEach(line => {
+    const match = re.exec(line)
+    if (match) {
+      stream.write(match.slice(1).join(',') + '\n')
+    }
+  })
+  stream.end()
+}()).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
